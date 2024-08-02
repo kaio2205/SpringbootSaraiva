7b0d7571740d6c84599d7267000e4405e2157fe1
Kaio Eduardo <kaiocarvalhode@gmail.com>
2024-08-02 16:43:21 -0300

    usando spring boot

--- src/main/java/br/com/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/projeto/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package br.com.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.domain.Usuario;
@@ -13,6 +16,7 @@ import br.com.projeto.repository.UsuarioRepository;
 
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 	@Autowired
 	UsuarioRepository ur;
@@ -29,6 +33,22 @@ public class UsuarioController {
 		
 		return ur.findAll();
 	}
+	
+	@GetMapping("/consultar/{id}")
+	public Optional<Usuario> consultar(@PathVariable Integer id) {
+		 return (ur).findById(id);
+		 
+	}
+	
+	@GetMapping("consultarusuario/{nomeusuario}")
+	public Optional<Usuario> consultarusuario(@PathVariable String nomeusuario) {
+		return ur.findByNomeusuario(nomeusuario);
+	}
+	
+	
+	
+	
+	
 	
 	
 	
